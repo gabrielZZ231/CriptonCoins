9b8db0b7d31709f97fabab204e65712cf9eb4502
RYAN VICTOR FERREIRA CONSTANTINO DA SILVA <20191D12GR0587@LABS.GARANHUNS.IFPE>
2022-05-30 17:02:57 -0300

    Implementação de ícones

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
 import { useState } from 'react';
-import { Input,Text } from 'react-native-elements';
-import { Feather as Icon} from '@expo/vector-icons';
+import { Input, Text } from 'react-native-elements';
+import { FontAwesome } from '@expo/vector-icons';
+import * as React from "react";
+import { Header } from 'react-native-elements';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+
 
 export default function App() {
 
@@ -11,34 +15,49 @@ export default function App() {
   const [text, setText] = useState('BTCUSDT')
   const [symbol, setSymbol] = useState('btcusdt')
 
-  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/${symbol}@ticker`,{
-      onMessage: () => {
-        if(lastJsonMessage) {
-            setData(lastJsonMessage)
-        }
-      },
-      onError: (event) => alert(event),
-      shouldReconnect: () => true,
-      reconnectInterval: 3000
+  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/${symbol}@ticker`, {
+    onMessage: () => {
+      if (lastJsonMessage) {
+        setData(lastJsonMessage)
+      }
+    },
+    onError: (event) => alert(event),
+    shouldReconnect: () => true,
+    reconnectInterval: 10000
   })
 
-  const searchButton = <Icon.Button
+ 
+  const searchButton = <FontAwesome.Button
   name="search"
   size={24}
   color="black"
   backgroundColor="transparent"
-  onPress={evt => setSymbol(text.toLowerCase()) }
-  />
-
+  onPress={evt => setSymbol(text.toLowerCase())}
+/>
+ const bitcoin = <Icon
+  rowing
+   name='bitcoin'
+   type='font-awesome' />
+  
+   const coin = <Icon 
+   rowing
+   name="hands-holding-dollar"
+   type='font-awesome'
+   /> 
   return (
     <View style={styles.container}>
-      <Text>CriptoCoins 1.0</Text>
+      <Header
+        backgroundColor='#8B0000'
+        placement="left"
+        leftComponent={bitcoin}
+        centerComponent={{ text: 'CriptoCoins 1.0', style: { color: 'black', fontWeight: "bold", fontSize: 20 } }}
+      />
       <Input
         autoCapitalize='characters'
-        leftIcon={<Icon name="dollar-sign" size={24} colors="black" />} 
+        leftIcon={hands-holding-dollar}
         rightIcon={searchButton}
-        value={text} 
-        onChangeText={setText}   
+        value={text}
+        onChangeText={setText}
       />
       <View style={styles.linha}>
         <Text style={styles.rotulo}>Preço Atual: </Text>
@@ -52,7 +71,7 @@ export default function App() {
         <Text style={styles.rotulo}>Volume: </Text>
         <Text style={styles.conteudo}>{data.v}</Text>
       </View>
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -60,12 +79,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-    marginTop: 40,
-    margin: 20,
-    alignContent: 'center'  
+    // marginTop: 40,
+    // margin: 20,
+    alignContent: 'center'
   },
   rotulo: {
-    fontWeight:'bold',
+    fontWeight: 'bold',
     fontSize: 24
   },
   conteudo: {
