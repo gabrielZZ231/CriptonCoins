bd5aed209eabbc64f88c32eb0baa72ef2cea7e8b
meu_username <meu_email>
2022-06-06 11:17:38 -0300

    Adição dos gráficos

--- App.js
+++ App.js
@@ -1,32 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Input, Text } from 'react-native-elements';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome, MaterialCommunityIcons  } from '@expo/vector-icons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import * as React from "react";
 import { Header } from 'react-native-elements';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { ScrollView } from 'react-native-gesture-handler';
+import { LineChart} from "react-native-chart-kit";
+import { Dimensions } from 'react-native'
 
 
 export default function App() {
+  
+  const [dataname, setDataName] = useState([]);
+  const [text, setText] = useState('BTCUSDT');
+  const [symbol, setSymbol] = useState('btcusdt');
+  const [coinsGrafichList, setcoinsGrafichList] = useState([0]);
+  const [days, setdays] = useState(180);
+  const [updateData, setupdateData] = useState(true);
 
-  const [data, setData] = useState({})
-  const [text, setText] = useState('BTCUSDT')
-  const [symbol, setSymbol] = useState('btcusdt')
-
-  const { lastJsonMessage } = useWebSocket(`wss://stream.binance.com:9443/ws/${symbol}@ticker`, {
-    onMessage: () => {
-      if (lastJsonMessage) {
-        setData(lastJsonMessage)
-      }
-    },
-    onError: (event) => alert(event),
-    shouldReconnect: () => true,
-    reconnectInterval: 10000
-  })
-
- 
   const searchButton = <FontAwesome.Button
   name="search"
   size={24}
@@ -35,15 +30,56 @@ export default function App() {
   onPress={evt => setSymbol(text.toLowerCase())}
 />
  const bitcoin = <Icon
-  rowing
+   rowing
    name='bitcoin'
    type='font-awesome' />
-  
-   const coin = <Icon 
-   rowing
-   name="hands-holding-dollar"
-   type='font-awesome'
-   /> 
+
+   const coin = <MaterialCommunityIcons
+    name="hand-coin" 
+    size={24} 
+    color="black" />
+
+
+
+   function addZero(number) {
+    if (number <= 9) {
+      return "0" + number;
+    }
+    return number;
+  }
+    const date = new Date();
+
+   function url(qtdDias) {
+    const listLastDays = qtdDias;
+    const end_date = 
+    `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+    date.setDate(date.getDate() - listLastDays);
+    const start_date = 
+    `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+    return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`;;
+  }
+
+  async function getPriceCoinsGraphic(url) {
+    let responseG = await fetch(url);
+    let returnApiG = await responseG.json();
+    let selectListQuotationsG = returnApiG.bpi;
+    const queryCoinsListG = Object.keys(selectListQuotationsG).map((key) => {
+      return selectListQuotationsG[key];
+    });
+    let dataG = queryCoinsListG;
+    return dataG;
+  }
+
+useEffect(() => {
+  getPriceCoinsGraphic(url(days)).then((dataG) => {
+    setcoinsGrafichList(dataG);
+  });
+  if (updateData) {
+    setupdateData(false);
+  }
+}, [updateData]);
+
+
   return (
     <View style={styles.container}>
       <Header
@@ -54,26 +90,66 @@ export default function App() {
       />
       <Input
         autoCapitalize='characters'
-        leftIcon={hands-holding-dollar}
+        leftIcon={coin }
         rightIcon={searchButton}
         value={text}
         onChangeText={setText}
       />
       <View style={styles.linha}>
-        <Text style={styles.rotulo}>Preço Atual: </Text>
-        <Text style={styles.conteudo}>{data.c}</Text>
+     {/* <Text>{dataname.vol}</Text> */}
+
+
+      <Text style={styles.rotulo}></Text>
+
+        {/* <Text style={styles.rotulo}>Preço Atual: </Text>
+        <Text style={styles.conteudo}>{data.c}</Text> */}
       </View>
       <View style={styles.linha}>
-        <Text style={styles.rotulo}>Variação %: </Text>
-        <Text style={styles.conteudo}>{data.P}%</Text>
+        {/* <Text style={styles.rotulo}>Variação %: </Text>
+        <Text style={styles.conteudo}>{data.P}%</Text> */}
       </View>
       <View style={styles.linha}>
-        <Text style={styles.rotulo}>Volume: </Text>
-        <Text style={styles.conteudo}>{data.v}</Text>
+        {/* <Text style={styles.rotulo}>Volume: </Text>
+        <Text style={styles.conteudo}>{data.v}</Text> */}
+      </View>
+      <View>
+      <LineChart   data={{
+        labels: ['Janeiro', 'Fevereiro', 'Março' ,'Abril' ,'Maio', 'Junho'],
+        datasets: [{
+          data: coinsGrafichList,
+        }]
+      }}
+      width={Dimensions.get('window').width} // from react-native
+      height={220}
+      yAxisLabel="$"
+        yAxisSuffix="k"
+        withVerticalLines={false}
+        yLabelsOffset={1}
+        withVerticalLabels={true}
+      chartConfig={{
+        backgroundColor: "#000000",
+        backgroundGradientFrom: "#232323",
+        backgroundGradientTo: "#3F3F3F",
+        decimalPlaces: 0, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        propsForDots: {
+          r: "1",
+          strokeWidth: "1",
+          stroke: "#f50d41",
+        },
+      }}
+      bezier
+      style={{
+        marginVertical: 8,
+        borderRadius: 16
+      }}
+  />
       </View>
       <StatusBar style="auto" />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
