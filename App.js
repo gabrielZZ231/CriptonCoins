fd078e8125f805cf73e752f9e7090aa54a257143
Ryan Víctor <ryannautico306@gmail.com>
2022-06-28 09:44:17 -0300

    Final app

--- App.js
+++ App.js
@@ -1,61 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Modal,Button } from 'react-native';
 import { useState, useEffect } from 'react';
-import { Input, Text } from 'react-native-elements';
-import { FontAwesome, MaterialCommunityIcons  } from '@expo/vector-icons';
+import {  Text } from 'react-native-elements';
 import * as React from "react";
 import { Header } from 'react-native-elements';
-import { Icon } from 'react-native-elements/dist/icons/Icon';
-import { Dimensions } from 'react-native'
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import { getMarketData } from './services/cryptoService';
 import Lista from './componentes/Lista_coins';
-import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
 export default function App() {
 
   const [data, setData] = useState([]);
   const [data1, setData1] = useState([]);
   const [modal,setmodal] = useState(false);
-  const [text, setText] = useState('BTC');
-  const [symbol, setSymbol] = useState('btc');
-  const [coinsGrafichList, setcoinsGrafichList] = useState([0]);
-  const [days, setdays] = useState(180);
-  const [busca,setbusca] = useState(" ");
-  const [updateData, setupdateData] = useState(true);
-
-  const searchButton = <FontAwesome.Button
-  name="search"
-  size={24}
-  color="black"
-  backgroundColor="transparent"
-  onPress={evt => setSymbol(text.toLowerCase())}
-/>
- const bitcoin = <Icon
-   rowing
-   name='bitcoin'
-   type='font-awesome' />
-
-   const coin = <MaterialCommunityIcons
-    name="hand-coin" 
-    size={24} 
-    color="black" />
-  
-   function addZero(number) {
-    if (number <= 9) {
-      return "0" + number;
-    }
-    return number;
-  }
-   function url(qtdDias) {
-    const date = new Date();
-    const listLastDays = qtdDias;
-    const end_date = 
-    `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
-    date.setDate(date.getDate() - listLastDays);
-    const start_date = 
-    `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
-    return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`;
-  }
 
   
   useEffect(() => {
@@ -77,16 +33,9 @@ const modalAbrir = (item) => {
       <Header
         backgroundColor='#8B0000'
         placement="left"
-        leftComponent={bitcoin}
         centerComponent={{ text: 'CriptoCoins 1.0', style: { color: 'black', fontWeight: "bold", fontSize: 20 } }}
       />
-      <Input
-        autoCapitalize='characters'
-        leftIcon={coin}
-        rightIcon={searchButton}
-        value={text}
-        onChangeText={setbusca}
-      />
+      
       <FlatList
         keyExtractor={(item) => item.id}
         data={data1}
@@ -107,8 +56,9 @@ const modalAbrir = (item) => {
           visible={modal}
         >
           <View style={styles.modal}>
-            <Text style={{ color: 'red' }}> saadasd {data.high_24h} </Text>
-            <Text style={{ color: 'red' }}> adsdsdas{data.low_24h}</Text>
+            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}> Alta nas últimas 24 horas: {data.high_24h} </Text>
+            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}> Baixa nas últimas 24 horas: {data.low_24h}</Text>
+            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}> Preço atual: {data.current_price}</Text>
           <Button
             title = "Fechar"
             onPress = {()=>{setmodal(false)}}
